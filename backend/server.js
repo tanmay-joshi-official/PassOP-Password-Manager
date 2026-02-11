a950f41c94331cad29ef9efdc281f25a35427116
Tanmay Joshi <tanmayjoshi579@gmail.com>
2026-02-12 00:56:16 +0530

    Refactor server.js to use async startServer function

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
+
 const express = require('express')
-const dotenv = require('dotenv')
-dotenv.config()
-const { MongoClient } = require('mongodb') 
+const { MongoClient } = require('mongodb')
 const bodyparser = require('body-parser')
 const cors = require('cors')
 const app = express()
@@ -11,48 +11,53 @@ app.use(cors())
 app.use(bodyparser.json())
 
 // Connection url
-const url = 'mongodb://localhost:27017/'
+const url = process.env.MONGO_URI
 const client = new MongoClient(url)
 
 // Database name
 const dbName = 'PassOP'
 
-client.connect()
 
-// Get all the passwords
-app.get('/', async (req, res) => {
-    const db = client.db(dbName)
-    const collection = db.collection('passwords')
-    const findResult = await collection.find({}).toArray()
-    res.json(findResult)
-})
+async function startServer() {
+    await client.connect()
 
-// Save a password
-app.post('/', async (req, res) => {
-    const password = req.body
-    const db = client.db(dbName)
-    const collection = db.collection('passwords')
-    const findResult = await collection.insertOne(password)
-    res.send({success: true, result: findResult})
-})
+    // Get all the passwords
+    app.get('/', async (req, res) => {
+        const db = client.db(dbName)
+        const collection = db.collection('passwords')
+        const findResult = await collection.find({}).toArray()
+        res.json(findResult)
+    })
 
-// Delete a password
-app.delete('/', async (req, res) => {
-    const password = req.body
-    const db = client.db(dbName)
-    const collection = db.collection('passwords')
-    const findResult = await collection.deleteOne(password)
-    res.send({success: true, result: findResult})
-})
+    // Save a password
+    app.post('/', async (req, res) => {
+        const password = req.body
+        const db = client.db(dbName)
+        const collection = db.collection('passwords')
+        const findResult = await collection.insertOne(password)
+        res.send({ success: true, result: findResult })
+    })
 
-// Deletes all passwords
-app.delete('/all', async (req, res) => {
-    const db = client.db(dbName)
-    const collection = db.collection('passwords')
-    const findResult = await collection.deleteMany({})
-    res.send({success: true, result: findResult})
-})
+    // Delete a password
+    app.delete('/', async (req, res) => {
+        const password = req.body
+        const db = client.db(dbName)
+        const collection = db.collection('passwords')
+        const findResult = await collection.deleteOne(password)
+        res.send({ success: true, result: findResult })
+    })
 
-app.listen(port, () => {
-    console.log(`App is ruunning at port ${port}`)
-})
+    // Deletes all passwords
+    app.delete('/all', async (req, res) => {
+        const db = client.db(dbName)
+        const collection = db.collection('passwords')
+        const findResult = await collection.deleteMany({})
+        res.send({ success: true, result: findResult })
+    })
+
+    app.listen(port, () => {
+        console.log(`App is ruunning at port ${port}`)
+    })
+}
+
+startServer()
